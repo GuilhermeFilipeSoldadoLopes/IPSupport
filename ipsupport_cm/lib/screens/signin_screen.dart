6d861815a64dc1caaf0167d067fc18b8fa2a99ce
GuilhermeFilipeSoldadoLopes <80033323+GuilhermeFilipeSoldadoLopes@users.noreply.github.com>
2023-07-03 14:13:51 +0100

    update

---
+++ ipsupport_cm/lib/screens/signin_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SingInScreen extends StatefulWidget {
+  const SingInScreen({super.key});
+
+  @override
+  _SingInScreenState createState() => _SingInScreenState();
+}
+
+class _SingInScreenState extends State<SingInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
