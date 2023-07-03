2e580008f945f43b0fe514eb1e417be57836f3a4
GuilhermeFilipeSoldadoLopes <80033323+GuilhermeFilipeSoldadoLopes@users.noreply.github.com>
2023-07-03 14:35:53 +0100

    update

--- ipsupport_cm/lib/main.dart
+++ ipsupport_cm/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ipsupport_cm/screens/signin_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        title: 'IPSupport',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: const SingInScreen());
   }
 }
--- ipsupport_cm/lib/screens/signin_screen.dart
+++ ipsupport_cm/lib/screens/signin_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SingInScreen extends StatefulWidget {
-  const SingInScreen({super.key});
+  const SingInScreen({Key? key}) : super(key: key);
 
   @override
   _SingInScreenState createState() => _SingInScreenState();
@@ -10,6 +10,6 @@ class SingInScreen extends StatefulWidget {
 class _SingInScreenState extends State<SingInScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold()
   }
 }
